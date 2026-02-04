edc7af323f56d8a711f0560677aba217cd47503a
unknown <josephcouture6@gmail.com>
2026-02-04 09:14:29 -0500

    updated env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE = "http://localhost:4000";
+export const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:4000";
 
 /** Layout persistence keys and pane limits */
 export const LAYOUT_KEYS = {
